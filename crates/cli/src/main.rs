f1dcac66296fd4324d641a97981e896a0dd9c162
34n0 <34n0@immerda.ch>
2024-01-19 21:53:41 +0100

    fix: cli print & rm unimplemented command

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
 use cmd::reset;
+use colored::Colorize;
+use std::fmt;
 use util::{log_error, log_info, syslog};
 mod cmd;
 
@@ -9,14 +11,60 @@ const BANNER: &str = r"
 ██   ████    ██   ██   ██   ████   ████   ██████  ██████   ██ 
 █████████    ██   ██   █████████████ █████████ ████ ████████  
 ██   ████    ██   ██   ██   ████   ████   ████  ██  ████      
-██   ██ ██████    ██   ██   ████   ████   ████      ████";
+██   ██ ██████    ██   ██   ████   ████   ████      ████
+
+by 34n0@immerda.ch";
 
 #[derive(Debug)]
 struct ArCliError {
     message: String,
 }
 
-type ArCliResult = Result<Option<String>, ArCliError>;
+impl fmt::Display for ArCliError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", "error:".red().bold(), self.message)
+    }
+}
+
+#[derive(Debug)]
+struct ArCliSuccess {
+    message: String,
+}
+
+impl fmt::Display for ArCliSuccess {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", "success:".green().bold(), self.message)
+    }
+}
+
+#[derive(Debug)]
+struct ArCliInfo {
+    message: String,
+}
+
+impl fmt::Display for ArCliInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", "info:".yellow().bold(), self.message)
+    }
+}
+
+#[derive(Debug)]
+enum ArCliResult {
+    Success(Option<ArCliSuccess>),
+    Info(ArCliInfo),
+    Error(ArCliError),
+}
+
+impl fmt::Display for ArCliResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ArCliResult::Success(Some(ref success)) => write!(f, "{success}"),
+            ArCliResult::Success(None) => Ok(()),
+            ArCliResult::Error(ref error) => write!(f, "{error}"),
+            ArCliResult::Info(ref info) => write!(f, "{info}"),
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(
@@ -32,8 +80,6 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    #[command(about = "Audit the PAM authramp log")]
-    Audit,
     #[command(about = "Reset a locked PAM user")]
     Reset {
         #[clap(long, short)]
@@ -46,19 +92,20 @@ fn main() {
 
     let cli_res = match Cli::parse().command {
         Some(Command::Reset { user }) => reset::user(&user),
-        _ => Ok(None),
+        _ => ArCliResult::Success(None),
     };
 
-    match cli_res {
-        Ok(res) => {
+    match &cli_res {
+        ArCliResult::Success(res) => {
             if let Some(res) = res {
-                log_info!("{}", &res);
-                println!("{}", &res);
+                log_info!("{}", &res.message);
             }
         }
-        Err(e) => {
-            log_error!("{}", &e.message);
-            println!("{}", &e.message);
+        ArCliResult::Error(res) => {
+            log_error!("{}", &res.message);
         }
+        ArCliResult::Info(_) => (),
     }
+
+    println!("{cli_res}");
 }
